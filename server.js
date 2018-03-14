991f5343f6ce0e50ae65f182d8144c9c96d9e555
ganiu <ganiu@ebay.com>
2018-03-14 11:11:22 -0700

    add crash me

--- server.js
+++ server.js
@@ -92,6 +92,10 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/crashme', function (req, res) {
+  process.exit();
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
